e7ea508904dea0d3dba0317e78c26285c6ef277f
NaN <121719158@qq.com>
2021-03-18 16:48:49 +0800

    controller简单单元测试

---
+++ src/test/java/com/automated/testing/controller/StudentControllerTest.java
@@ -0,0 +1,77 @@
+package com.automated.testing.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class StudentControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private StudentController studentController;
+
+    @Test
+    public void testNotNull() {
+        assertThat(studentController, notNullValue());
+    }
+
+    @Test
+    public void getStudentById() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/students/{id}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("name", is("wh")))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void listStudents() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/students")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    void insertStudent() throws Exception {
+        String requestBody = "{\"id\":1, \"name\":\"wh\"}";
+        mvc.perform(MockMvcRequestBuilders.post("/students")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void updateStudent() throws Exception {
+        String requestBody = "{\"id\":1, \"name\":\"wh\"}";
+        mvc.perform(MockMvcRequestBuilders.put("/students")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void deleteStudent() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.delete("/students/{id}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
