17c1e082107825e016f15d6267c3e75027a91c48
NaN <121719158@qq.com>
2021-03-18 15:40:19 +0800

    修改controller方法

--- src/main/java/com/automated/testing/controller/StudentController.java
+++ src/main/java/com/automated/testing/controller/StudentController.java
@@ -1,14 +1,16 @@
 package com.automated.testing.controller;
 
+import com.automated.testing.model.Student;
 import com.automated.testing.service.StudentService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author reeedking
- * @date  2021/3/18
- * @description 
+ * @date 2021/3/18
+ * @description
  */
 @RestController
 @RequestMapping("/students")
@@ -17,23 +19,29 @@ public class StudentController {
     @Resource
     private StudentService studentService;
 
-    @GetMapping
-    public Object getDemo(){
-        return "test get request";
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable String id) {
+        return studentService.getStudentById(id);
     }
 
+    @GetMapping
+    public List<Student> listStudents() {
+        return studentService.getStudents();
+    }
+
+
     @PostMapping
-    public Object postDemo(){
-        return "test post request";
+    public int insertStudent(@RequestBody Student student) {
+        return studentService.insertStudent(student);
     }
 
     @PutMapping
-    public Object putDemo(){
-        return "test put request";
+    public int updateStudent(@RequestBody Student student) {
+        return studentService.updateStudent(student);
     }
 
-    @DeleteMapping
-    public Object deleteDemo(){
-        return "test delete request";
+    @DeleteMapping("/{id}")
+    public int deleteStudent(@PathVariable String id) {
+        return studentService.deleteStudentById(id);
     }
 }
