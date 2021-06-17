38514c7a2908aa8cde5f867cf3f17b0233b4b17b
reeedking <121719158@qq.com>
2021-06-17 20:24:33 +0800

    测试环境

--- src/main/java/com/automated/testing/mapper/UserMapper.java
+++ src/main/java/com/automated/testing/mapper/UserMapper.java
@@ -3,6 +3,5 @@ package com.automated.testing.mapper;
 import com.automated.testing.model.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
-// test
 public interface UserMapper extends BaseMapper<User> {
 }
