83ed3002aff33269f17783b22d491317110260ab
reeedking <121719158@qq.com>
2021-06-24 16:14:32 +0800

    试用junit5 断言

--- src/test/java/junit5/Calculator.java
+++ src/test/java/junit5/Calculator.java
@@ -5,4 +5,14 @@ public class Calculator {
     public int add(int num1, int num2) {
         return num1 + num2;
     }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public double divide(int num1, int num2) {
+        if (num2 == 0)
+            throw new ArithmeticException();
+        return num1 / num2;
+    }
 }
---
+++ src/test/java/junit5/Person.java
@@ -0,0 +1,15 @@
+package junit5;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    private String firstName;
+
+    private String lastName;
+}
